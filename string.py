95ad9a0a3336f1a7e7bf1a3ded2998f6620ca762
harikakatta627 <167598132+harikakatta627@users.noreply.github.com>
2025-11-12 20:50:27 +0530

    first commit

---
+++ string.py
@@ -0,0 +1,15 @@
+# exaple 1
+a="Hello"
+b='World'                   
+print(type(a))
+print(type(b))
+print("Concatenated String : ",a+b)
+# print("string : ",a+ " " + b)
+# print("length of a : ",len(a))
+# print("length of b : ",len(b))
+# print("length of a+b : ",len(a+b))
+# print("Uppercase a : ",a.upper())
+# print("Lowercase b : ",b.lower())               
+# print("Replace in a : ",a.replace("H","h"))
+# print("Replace in b : ",b.replace("W","s"))
+# print("Split a : ",a.split("e"))
