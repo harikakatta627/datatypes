5872bea8bba08582a013e2e7d7aac4564dfae1f4
harikakatta627 <167598132+harikakatta627@users.noreply.github.com>
2025-11-12 20:15:43 +0530

    numeric

---
+++ int.py
@@ -0,0 +1,25 @@
+#example 1
+    
+a=10 
+b=5
+print(type(a))
+print(type(b))
+print("sum is : ",a+b)
+
+
+#example 2
+
+c=3.5
+d=2.5       
+print(type(c))
+print(type(d))
+print("sum is : ",c+d)
+
+
+#example 3
+
+e= 1+2j
+f= 3+4j
+print(type(e))
+print(type(f))      
+print("sum is : ",e+f)
