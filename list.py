02ad246f19b195569e8894746eb8ebb3a7230718
harikakatta627 <167598132+harikakatta627@users.noreply.github.com>
2025-11-12 21:03:45 +0530

    first commit

---
+++ list.py
@@ -0,0 +1,16 @@
+# example
+my_list = [1, 2, 3, 4, 5]
+print(type(my_list))
+print("Original List : ", my_list)
+print("Accessing elements:")
+print("First element:", my_list[0])
+print("Last element:", my_list[-1]) 
+print("Slicing List : ", my_list[1:4])
+print("Length of List : ", len(my_list))    
+print("Appending 6 to List")
+my_list.append(6)
+print("Updated List : ", my_list)   
+print("Removing 2 from List")
+my_list.remove(2)
+print("Final List : ", my_list)
+print("Reversed List : ", my_list[::-1])
